74d342beb0519f1696b4f3a6dff7b9f32a808812
Kometani Yuuka <p000527465@rfg.rfgad.ricoh.com>
2019-09-21 22:24:28 +0900

    clock replace almost center of window/change color

--- dc/src/dc1_1/DigitalClock.java
+++ dc/src/dc1_1/DigitalClock.java
@@ -1,34 +1,69 @@
 package dc1_1;
 
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.FontMetrics;
 import java.awt.Frame;
 import java.awt.Graphics;
+import java.awt.Rectangle;
+import java.awt.event.ComponentEvent;
+import java.awt.event.ComponentListener;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
 import java.util.Calendar;
 
+import javax.swing.JOptionPane;
 
-public class DigitalClock extends Frame implements Runnable,WindowListener{
+
+public class DigitalClock extends Frame implements Runnable,WindowListener,ComponentListener{
 	int width = 500, height = 200;
 
-	Calendar now;
-	String h,m,s;
-	String nowStr = "";
+	/**
+	 * 時間を保持
+	 */
+	private Calendar date;
+	private String hhmmss;
+	private String yyyymmdd;
+
+
+    public static void main(String[] args) {
+    	JOptionPane.showMessageDialog(null, "ディレクトリ構成にsrcはいらないぞ");
+    	DigitalClock dc = new DigitalClock();
+		Thread thread = new Thread(dc);
+		thread.start();
+    }
+
 	public DigitalClock() {
+		init();
 		setSize(width, height);
 		addWindowListener(this);
+		addComponentListener(this);
 		setVisible(true);
 	}
 
-    public void paint(Graphics g)
-    {
-        g.drawString(nowStr,100,100);
-    }
+	/**
+	 * 初期値の設定
+	 */
+	public void init() {
+		// get now time
+		date = Calendar.getInstance();
+		// 1,2. フォント、サイズの指定
+		Font font = new Font(Font.MONOSPACED,Font.BOLD,50);
+		setFont(font);
+		// 3. 文字色の指定
+		setForeground(Color.DARK_GRAY);
+		// 4. 時計の背景色の指定
+		setBackground(Color.WHITE);
+	}
 
+	/**
+	 * スレッドの実行処理
+	 */
 	@Override
 	public void run() {
 		while (true) {
 			// get now time
-			nowStr = timeToString();
+			date = Calendar.getInstance();
 			repaint();
 			try {
 				// wait 1/10 second
@@ -39,23 +74,50 @@ public class DigitalClock extends Frame implements Runnable,WindowListener{
 			}
 		}
 	}
+
+	/**
+	 * 描画メソッド
+	 */
+	public void paint(Graphics g) {
+		hhmmss = timeToString();
+		yyyymmdd = dateToString();
+		int fontSize = getFont().getSize();
+		drawStringCenter(g,hhmmss,   width/2, height/2);
+		drawStringCenter(g,yyyymmdd, width/2, height/2+fontSize);//ascent使った方がいいの？
+	}
+
+	// accessor
+
+	// utility
 	public String timeToString() {
-		now = Calendar.getInstance();
-		h = convertTwoDigitString(now.get( Calendar.HOUR_OF_DAY ));
-		m = convertTwoDigitString(now.get( Calendar.MINUTE ));
-		s = convertTwoDigitString(now.get( Calendar.SECOND ));
+		String h = convertDigitString(date.get( Calendar.HOUR_OF_DAY ),2);
+		String m = convertDigitString(date.get( Calendar.MINUTE ),2);
+		String s = convertDigitString(date.get( Calendar.SECOND ),2);
 		return h+":"+m+":"+s;
 	}
-	public String convertTwoDigitString(int num) {
-		return String.format("%02d", num);
+	public String dateToString() {
+		String yyyy = convertDigitString(date.get( Calendar.YEAR ),4);
+		String mm = convertDigitString(date.get( Calendar.MONTH ),2);
+		String dd = convertDigitString(date.get( Calendar.DAY_OF_MONTH ),2);
+		return yyyy+"/"+mm+"/"+dd;
+	}
+	public String convertDigitString(int num,int digit) {
+		// digitを越えた桁を削除
+		num %= (int)( Math.pow(10,digit));
+		String format = "%0"+digit+"d";
+		return String.format(format, num);
 	}
 
-    public static void main(String[] args) {
-    	DigitalClock dc = new DigitalClock();
-		Thread thread = new Thread(dc);
-		thread.start();
-    }
+	public static void drawStringCenter(Graphics g,String text,int x,int y){
+		FontMetrics fontMet = g.getFontMetrics();
+		Rectangle textRect = fontMet.getStringBounds(text, g).getBounds();
+		int cx = x-textRect.width/2;
+		int cy = y-textRect.height/2+fontMet.getMaxAscent();
 
+        g.drawString(text, cx, cy);
+	}
+
+	// WindowListener
 	@Override
 	public void windowOpened(WindowEvent e) {}
 	@Override
@@ -72,5 +134,18 @@ public class DigitalClock extends Frame implements Runnable,WindowListener{
 	public void windowActivated(WindowEvent e) {}
 	@Override
 	public void windowDeactivated(WindowEvent e) {}
+
+	// ComponentListener
+	@Override
+	public void componentResized(ComponentEvent e) {
+		width = getWidth();
+		height = getHeight();
+	}
+	@Override
+	public void componentMoved(ComponentEvent e) {}
+	@Override
+	public void componentShown(ComponentEvent e) {}
+	@Override
+	public void componentHidden(ComponentEvent e) {}
 
 }
