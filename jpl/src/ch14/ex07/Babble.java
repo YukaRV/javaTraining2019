5ded1a64b3343a111266cd77ce5366cd75e07674
unknown <yk.1218.blackcat@gmail.com>
2020-01-09 21:23:34 +0900

    ch14.ex8まで追加(調査中)

--- jpl/src/ch14/ex07/Babble.java
+++ jpl/src/ch14/ex07/Babble.java
@@ -3,7 +3,17 @@ package ch14.ex07;
 // 14.7
 // Babbleを複数回実行して、結果を調べなさい。常に同じ結果ですか。
 // 可能なら、異なるシステムで実行して比較しなさい。
-// TODO 比較
+// Did Did DidNot DidNot
+// Did DidNot DidNot Did
+// Did Did DidNot DidNot
+// Did Did DidNot DidNot
+// Did Did DidNot DidNot
+// Did DidNot DidNot Did
+// Did Did DidNot DidNot
+// DidNot DidNot Did Did
+// Did DidNot DidNot Did
+// Did Did DidNot DidNot
+// 10回中6回が同じ結果(Did Did DidNot DidNot)
 
 public class Babble extends Thread {
 	static boolean doYield;	// 他のスレッドに実行を譲るか？
